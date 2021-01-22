656c01545f37a3529a1a8124a48a053f450c0df1
noobertx <noobertx@gmail.com>
2021-01-22 21:01:37 +0800

    Trimmed renderfield for image grid

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '6b832eb9c588f8a812957436e0d4c836');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'ba02977b74fe0d418d4ba2fb638162ae');
