13145ef8a5c40117a5fb1bcdb1796e22fc571230
noobertx <noobertx@gmail.com>
2021-03-08 02:36:16 +0800

    Counter Block Issue

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,4 +1,14 @@
 (function($){
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
     $(document).ready(function() {
         if($(".wprig-gallery").length){
             $(".wprig-gallery").slick()
@@ -50,6 +60,34 @@
                 e.preventDefault();
                 $(this).closest(".wp-block-wprig-alert").remove();
             })
+        }
+
+        if(true){
+            $('.wprig-block-counter-number').each(function () {
+                const currentElement = $(this)[0];
+                let start = parseInt(currentElement.dataset.start),
+                    limit = parseInt(currentElement.dataset.limit),
+                    counterDuration = parseInt(currentElement.dataset.counterduration),
+                    increment = Math.ceil((limit / counterDuration) * 10);
+    
+                const invokeCounter = () => {
+                    if (isElementInViewport(currentElement)) {
+                        if (start < limit) {
+                            let intervalId = setInterval(function () {
+                                let difference = limit - start;
+                                difference >= increment ? start += increment : difference >= 50 ? start += 50 : start++;
+                                currentElement.innerText = start;
+                                if (start >= limit) {
+                                    clearInterval(intervalId);
+                                }
+                            }, 10);
+                        }
+                        window.removeEventListener('scroll', invokeCounter, true);
+                    }
+                }
+                invokeCounter();
+                window.addEventListener('scroll', invokeCounter, true);
+            });
         }
     });
 })(jQuery)
