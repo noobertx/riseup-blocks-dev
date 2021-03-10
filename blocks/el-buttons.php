fd4c785cd554320253f9a13bfc7ef69c1675f9b8
noobertx <noobertx@gmail.com>
2021-03-11 05:05:52 +0800

    Simple Button Frontend revision

--- blocks/el-buttons.php
+++ blocks/el-buttons.php
@@ -104,24 +104,22 @@ function render_block_el_button($att)
 	$buttonWidthType 		= isset($att['buttonWidthType']) ? $att['buttonWidthType'] : "auto";
 	$iconPosition 		    = isset($att['iconPosition']) ? $att['iconPosition'] : "auto";
 	$iconName 		   		= isset($att['iconName']) ? $att['iconName'] : "auto";
-	$url 				    = isset($att['url']) ? $att['url'] : "#";
-
+	$url 				    = isset($att['url']) ? $att['url']['url'] : "#";
+	$html = [];
 	$html =  "<div class='".$classname."' {...animationAttr(animation)}>";
-	$html .= "<div class='wprig-block-btn-wrapper  wprig-btn-".$alignment['md']."'>";
-	$html .= "<div class='wprig-block-btn'>";
-	$html .= "<a href = '".$url."' class='wprig-block-btn-anchor is-".$buttonSize." ".$buttonColor." wprig-btn-".$buttonWidthType."'>";
-	if($iconPosition=="left"){
-		$html .= "<i class='wprig-btn-icon ".$iconName."'></i>";
-		
-	}
-	$html .=$textField;
-	if($iconPosition=="right"){
-		$html .= "<i class='wprig-btn-icon ".$iconName."'></i>";
-	}
-
-	$html .= "</a>";
-	$hmtl .= "</div>";
-	$hmtl .= "</div>";
+		$html .= "<div class='wprig-block-btn-wrapper  wprig-btn-".$alignment['md']."'>";
+			$html .= "<div class='wprig-block-btn'>";
+			$html .= "<a href = '".$url."' class='wprig-block-btn-anchor is-".$buttonSize." ".$buttonColor." wprig-btn-".$buttonWidthType."'>";
+			if($iconPosition=="left"){
+				$html .= "<i class='wprig-btn-icon ".$iconName."'></i>";
+			}
+			$html .=$textField;
+			if($iconPosition=="right"){
+				$html .= "<i class='wprig-btn-icon ".$iconName."'></i>";
+			}
+			$html .= "</a>";
+		$html .= "</div>";
+	$html .= "</div>";
 	$html .= "</div>";
 
 	/*
@@ -140,5 +138,5 @@ function render_block_el_button($att)
 	return $html;
 }
 
-add_action('init', 'register_block_el_button', 100);
+add_action('init', 'register_block_el_button', 250);
 
